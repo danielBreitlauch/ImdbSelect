39f9b94e7b9e2579499af4e0600fbdebb9cde574
danielBreitlauch <github@flying-stampe.de>
2018-08-10 20:11:48 +0200

    allow actresses ;/

--- Imdb.py
+++ Imdb.py
@@ -5,7 +5,7 @@ class Imdb:
     def __init__(self):
         self.ia = IMDb()
 
-    def imdb_id_list_from_person(self, name, role='actor'):
+    def imdb_id_list_from_person(self, name, role='actx'):
         persons = self.ia.search_person(name)
         if len(persons) == 0:
             return []
@@ -14,7 +14,11 @@ class Imdb:
 
         ids = []
         for kind in full_person['filmography']:
-            if role in kind:
-                for movie in kind[role]:
-                    ids.append('tt' + movie.movieID)
+            if role == 'actx':
+                if 'actor' in kind:
+                    for movie in kind['actor']:
+                        ids.append('tt' + movie.movieID)
+                if 'actress' in kind:
+                    for movie in kind['actress']:
+                        ids.append('tt' + movie.movieID)
         return ids
